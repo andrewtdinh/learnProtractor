ee1095725ee79fe07f3591fc16d965607c88d19b
Andrew Dinh <andrewdinh74@yahoo.com>
2015-10-31 23:22:27 -0700

    Fix the simple calculator addition test

--- calc_spec.js
+++ calc_spec.js
@@ -2,12 +2,12 @@
 describe('Protractor Demo App', function() {
   it('should add one and two', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+    element(by.model('first')).sendKeys(6);
+    element(by.model('second')).sendKeys(7);
 
     element(by.id('gobutton')).click();
 
     expect(element(by.binding('latest')).getText()).
-        toEqual('5'); // This is wrong!
+        toEqual('13'); // This is wrong!
   });
 });
