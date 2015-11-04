8fa58d9bfd0feaddd8728c5c5544f15d908bf897
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-04 00:44:31 -0800

    Add a spec that uses page object

---
+++ specs/pageObject_shortExample_spec.js
@@ -0,0 +1,10 @@
+describe('angularjs homepage', function() {
+  it('should greet the named user', function() {
+    var angularHomepage = new AngularHomepage();
+    angularHomepage.get();
+
+    angularHomepage.setName('Julie');
+
+    expect(angularHomepage.getGreeting()).toEqual('Hello Julie!');
+  });
+});
