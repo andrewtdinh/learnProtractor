86359dd0c6f8af73c211713be311a6d7f445d4f8
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-05 22:25:45 -0800

    Change the page object variables slightly

--- specs/pageObject_shortExample_spec.js
+++ specs/pageObject_shortExample_spec.js
@@ -1,8 +1,8 @@
-var AngularHomepage = require('../pages/angular_home')
+var AngularHome = require('../pages/angular_home');
 
 describe('angularjs homepage', function() {
   it('should greet the named user', function() {
-    var angularHomepage = new AngularHomepage();
+    var angularHomepage = new AngularHome();
     angularHomepage.get();
 
     angularHomepage.setName('Julie');
