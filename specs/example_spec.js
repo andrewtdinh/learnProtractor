9c2af9add1ff887be6876423158e2430bab6270c
Andrew Dinh <andrewdinh74@yahoo.com>
2015-11-06 23:32:17 -0800

    Remove a redundant test from the example spec

--- specs/example_spec.js
+++ specs/example_spec.js
@@ -1,37 +1,27 @@
-describe('angularjs homepage', function() {
-  it('should greet the named user', function() {
+// You can have more describe blocks in this file.
+
+describe('todo list', function() {
+  var todoList;
+
+  beforeEach(function() {
     browser.get('http://www.angularjs.org');
 
-    element(by.model('yourName')).sendKeys('Julie');
-
-    var greeting = element(by.binding('yourName'));
-
-    expect(greeting.getText()).toEqual('Hello Julie!');
+    todoList = element.all(by.repeater('todo in todoList.todos'));
   });
 
-  describe('todo list', function() {
-    var todoList;
+  it('should list todos', function() {
+    expect(todoList.count()).toEqual(2);
+    expect(todoList.get(1).getText()).toEqual('build an angular app');
+  });
 
-    beforeEach(function() {
-      browser.get('http://www.angularjs.org');
+  it('should add a todo', function() {
+    var addTodo = element(by.model('todoList.todoText'));
+    var addButton = element(by.css('[value="add"]'));
 
-      todoList = element.all(by.repeater('todo in todoList.todos'));
-    });
+    addTodo.sendKeys('write a protractor test');
+    addButton.click();
 
-    it('should list todos', function() {
-      expect(todoList.count()).toEqual(2);
-      expect(todoList.get(1).getText()).toEqual('build an angular app');
-    });
-
-    it('should add a todo', function() {
-      var addTodo = element(by.model('todoList.todoText'));
-      var addButton = element(by.css('[value="add"]'));
-
-      addTodo.sendKeys('write a protractor test');
-      addButton.click();
-
-      expect(todoList.count()).toEqual(3);
-      expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write a protractor test');
   });
 });
